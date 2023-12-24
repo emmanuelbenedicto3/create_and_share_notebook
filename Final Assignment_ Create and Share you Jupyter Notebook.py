5c5c3331212ece5066c1d444f11ee236e8e4fa6a
emmanuelbenedicto3 <153699316+emmanuelbenedicto3@users.noreply.github.com>
2023-12-24 14:05:45 +0800

    Add files via upload

---
+++ Final Assignment_ Create and Share you Jupyter Notebook.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# **Objectives:**
+# - To list popular languages for data science
+# - To enumerate some of the most commonly used libraries for data science
+# - To identify open-source data science tools
+
+# Some of the popular languages that Data Scientists use are:
+# 1. Python
+# 2. R
+# 3. SQL
+# 
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 1. ggplot
+# 2. matplotlib
+# 3. caret
+# 4. dplyr
+# 5. tensorflow
+
+# |Data Science Tools |
+# |---|
+# |Jupyter|
+# |GitHub|
+# |Spark|
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[9]:
+
+
+# This is a simple arithmetic expression
+(3*4)+5
+
+
+# In[8]:
+
+
+# This will convert 200 minutes to hours by dividing by 60
+200/60
+
+
+# ## Author
+# Emmanuel B. Benedicto
+
+# In[ ]:
+
+
+
+
